70999dbcb4970cd7d1cf5bb263e2594b4b2278fd
Erik Pedersen <sayro_naught@hotmail.com>
2025-08-05 08:29:22 +0200

    VRE now correctly set up, only 3rd attempt.. lol

---
+++ Source/UE_VrArcadePrototype/EmptyClassForProject.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//#include "UE_VrArcadePrototype.h"
+#include "EmptyClassForProject.h"
+
+EmptyClassForProject::EmptyClassForProject()
+{
+}
+
+EmptyClassForProject::~EmptyClassForProject()
+{
+}
---
+++ Source/UE_VrArcadePrototype/UE_VrArcadePrototype.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE_VrArcadePrototype.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_VrArcadePrototype, "UE_VrArcadePrototype" );
