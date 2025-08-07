a81d500bee64f80cf9bc13ca167b2891f814cd19
sayronaught <sayro_naught@hotmail.com>
2025-08-07 22:47:57 +0200

    Climbing momentum and theowing strength from acutla character, arm stamina preparation

--- Source/UE_VrArcadePrototype/UE_VrArcadePrototype.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "UE_VrArcadePrototype.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_VrArcadePrototype, "UE_VrArcadePrototype" );
